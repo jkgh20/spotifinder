dcc0a867066ba35e37b08250bacc81188aec9590
jkgh20 <jkgh20@users.noreply.github.com>
2019-10-24 21:33:31 -0500

    Find genre values

--- api/main.go
+++ api/main.go
@@ -32,6 +32,7 @@ type SeatGeekEvent struct {
 	eventType     string
 	url           string
 	performers    []string
+	genres        []string //possibly nil
 	localShowtime string
 	venueName     string
 	venueLocation string
@@ -144,7 +145,7 @@ func GeneratePlayList(client spotify.Client, playlistName string, description st
 }
 
 func findLocalConcerts() []SeatGeekEvent {
-	testURL := "https://api.seatgeek.com/2/events?client_id=" + SEATGEEK_ID + "&geoip=78745&range=4mi"
+	testURL := "https://api.seatgeek.com/2/events?client_id=" + SEATGEEK_ID + "&geoip=78745&range=20mi"
 
 	resp, err := http.Get(testURL)
 	if err != nil {
@@ -162,7 +163,7 @@ func findLocalConcerts() []SeatGeekEvent {
 
 	var responseData map[string]interface{}
 
-	err = json.Unmarshal(body, &responseData) //Fields are empty?
+	err = json.Unmarshal(body, &responseData)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
 	}
@@ -189,18 +190,45 @@ func findLocalConcerts() []SeatGeekEvent {
 		for _, performer := range performersArray {
 			performerData := performer.(map[string]interface{})
 			seatGeekEvents[i].performers = append(seatGeekEvents[i].performers, performerData["short_name"].(string))
+			seatGeekEvents[i].genres = GetSeatGeekArtistGenres(fmt.Sprintf("%d", int(performerData["id"].(float64))))
 		}
 	}
 
 	return seatGeekEvents
 }
 
-func getJson(url string, target interface{}) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
+func GetSeatGeekArtistGenres(performerId string) []string {
+	SeatGeekPerformerURL := "https://api.seatgeek.com/2/performers/" + performerId + "?client_id=" + SEATGEEK_ID
+	fmt.Printf(SeatGeekPerformerURL)
 
-	return json.NewDecoder(resp.Body).Decode(target)
+	resp, err := http.Get(SeatGeekPerformerURL)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		return nil
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		return nil
+	}
+
+	var responseData map[string]interface{}
+
+	err = json.Unmarshal(body, &responseData)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		return nil
+	}
+
+	var genreArray []string
+
+	if genresFromResponse, keyExists := responseData["genres"].([]interface{}); keyExists {
+		for _, genre := range genresFromResponse {
+			genreData := genre.(map[string]interface{})
+			genreArray = append(genreArray, genreData["slug"].(string))
+		}
+	}
+
+	return genreArray
 }
