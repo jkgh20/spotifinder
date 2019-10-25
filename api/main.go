d9546703ddbdd685389b6f97fbeeabf778d8c2be
jkgh20 <jkgh20@users.noreply.github.com>
2019-10-24 23:47:03 -0500

    Search for top tracks; generate playlist

--- api/main.go
+++ api/main.go
@@ -43,18 +43,28 @@ func main() {
 	router.HandleFunc("/", Index)
 	router.HandleFunc("/authenticate", Authenticate)
 	router.HandleFunc("/callback", Callback)
+	router.HandleFunc("/test", test)
 	log.Fatal(http.ListenAndServe(":"+applicationPort, router))
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	findLocalConcerts()
-	/*
-		GetAndPrintProfileData(spotifyClient)
 
-		GeneratePlayList(spotifyClient, "testing the waters", "a playlist to test the waters. duh!")
+}
 
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	*/
+func test(w http.ResponseWriter, r *http.Request) {
+	localSeatGeekEvents := findLocalEvents()
+
+	playlistID := GeneratePlayList(spotifyClient, "Best playlist!", "Desc")
+
+	for _, event := range localSeatGeekEvents {
+		if event.eventType == "concert" || event.eventType == "music_festival" {
+			for _, performer := range event.performers {
+				artistID := SearchAndFindSpotifyArtistID(spotifyClient, performer)
+				topTracks := GetTopFourSpotifyArtistTracks(spotifyClient, artistID)
+				AddTracksToPlaylist(spotifyClient, playlistID, topTracks)
+			}
+		}
+	}
 }
 
 func Callback(w http.ResponseWriter, r *http.Request) {
@@ -83,68 +93,69 @@ func ObtainAuthenticationURL(state string) string {
 	return spotifyAuth.AuthURL(state)
 }
 
-func GetAndPrintProfileData(client spotify.Client) {
-	flag.Parse()
-
-	if *userID == "" {
-		fmt.Fprintf(os.Stderr, "Error: missing user ID\n")
-		flag.Usage()
-		return
-	}
-
-	user, err := client.GetUsersPublicProfile(spotify.ID(*userID))
-
+func GetTopFourSpotifyArtistTracks(client spotify.Client, artistID spotify.ID) []spotify.FullTrack {
+	topTracks, err := spotifyClient.GetArtistsTopTracks(artistID, "US")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
-		return
-	}
+		return nil
+	} else {
+		var topFourTracks []spotify.FullTrack
 
-	fmt.Println("User ID:", user.ID)
-	fmt.Println("Display name:", user.DisplayName)
-	fmt.Println("Spotify URI:", string(user.URI))
-	fmt.Println("Endpoint:", user.Endpoint)
-	fmt.Println("Followers:", user.Followers.Count)
+		for i, track := range topTracks {
+			topFourTracks = append(topFourTracks, track)
+			if i == 3 {
+				break
+			}
+		}
+		return topFourTracks
+	}
 }
 
-func GeneratePlayList(client spotify.Client, playlistName string, description string) {
+func GeneratePlayList(client spotify.Client, playlistName string, description string) spotify.ID {
 	flag.Parse()
 
 	if *userID == "" {
 		fmt.Fprintf(os.Stderr, "Error: missing user ID\n")
 		flag.Usage()
-		return
+		return ""
 	}
 
 	playList, err := client.CreatePlaylistForUser(*userID, playlistName, description, true)
-
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
-		return
+		return ""
 	} else {
 		fmt.Printf("Created playlist %s for user %s", playlistName, *userID)
 	}
 
-	//var tracksToAdd []spotify.SimpleTrack
-	var trackIDsToAdd []spotify.ID
+	return playList.ID
+}
 
-	//Dummy data
-	trackIDsToAdd = append(trackIDsToAdd, "6rPO02ozF3bM7NnOV4h6s2") //Despacito
-	trackIDsToAdd = append(trackIDsToAdd, "6rPO02ozF3bM7NnOV4h6s2")
-
-	for _, trackID := range trackIDsToAdd {
-
-		_, err := client.AddTracksToPlaylist(playList.ID, trackID)
-
+func AddTracksToPlaylist(client spotify.Client, playlistID spotify.ID, tracksToAdd []spotify.FullTrack) {
+	for _, track := range tracksToAdd {
+		_, err := client.AddTracksToPlaylist(playlistID, track.ID)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, err.Error())
 			return
-		} else {
-			fmt.Printf("Added track ID %s to playlist ID %s", trackID, playList.ID)
 		}
 	}
 }
 
-func findLocalConcerts() []SeatGeekEvent {
+func SearchAndFindSpotifyArtistID(client spotify.Client, artistName string) spotify.ID {
+	searchResults, err := client.Search(artistName, spotify.SearchTypeArtist)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, err.Error())
+		return ""
+	} else {
+		if len(searchResults.Artists.Artists) != 0 {
+			return searchResults.Artists.Artists[0].ID
+		}
+	}
+
+	return ""
+}
+
+func findLocalEvents() []SeatGeekEvent {
 	testURL := "https://api.seatgeek.com/2/events?client_id=" + SEATGEEK_ID + "&geoip=78745&range=20mi"
 
 	resp, err := http.Get(testURL)
