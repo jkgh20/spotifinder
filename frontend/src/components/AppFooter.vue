e487e53e944c7592757275245f7812e2359797a0
jkgh20 <jkgh20@users.noreply.github.com>
2019-11-28 23:53:59 -0500

    Added component files

---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="footer">
+      Powered by <a href="https://seatgeek.com/"><img src="../assets/seatgeek-logo.png"></a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'appFooter'
+}
+</script>
---
+++ frontend/src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">Otherside</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+
+        <!-- Right aligned nav items -->
+
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item href="#">Link</b-nav-item>
+          <b-nav-item href="#">Disabled</b-nav-item>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template v-slot:button-content>
+              My Account
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'appHeader'
+}
+</script>
