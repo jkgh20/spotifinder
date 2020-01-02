9c08f220e519af9ff5948adc40c024a1317a200e
jkgh20 <jkgh20@users.noreply.github.com>
2020-01-02 16:06:55 -0600

    Navbar account name; page margins

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -9,19 +9,15 @@
         <!-- Right aligned nav items -->
 
         <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
+          <b-nav-item right>
             <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
               <span v-if="currentUser"> 
                 {{currentUser}}
               </span>
               <span v-else>
                 My Account
               </span>
-            </template>
-            <b-dropdown-item href="#">Profile</b-dropdown-item>
-            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-          </b-nav-item-dropdown>
+          </b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
